2180f6860334f6b945c4c3031fcec02e83d9b80c
Alex Prylypko <oleksandr.prylypko@kitrum.com>
2022-06-21 15:07:57 +0300

    Add counter

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,33 @@ import { Button } from "@material-ui/core";
 import RippleAnimation from "./ripple-animation";
 
 const App: React.FC<{}> = () => {
+  const waitValue = 10;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (counter === 0) {
+      return;
+    }
+    setTimeout(() => {
+      setCounter((preCounter) => preCounter - 1);
+    }, 1000);
+  }, [counter]);
+
   return (
     <>
       <RippleAnimation />
+      <div className="App">
+        <h1>Please wait {counter}</h1>
+        <Button
+          variant="contained"
+          disabled={counter > 0}
+          onClick={() => {
+            setCounter(waitValue);
+          }}
+        >
+          Run
+        </Button>
+      </div>
     </>
   );
 };
